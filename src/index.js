6e06ad5dba30fe8bb62a8f9bd0d73181d0bd36d2
Your Name <your@email.com>
2025-06-01 19:38:08 +0000

    Connected to MongoAtlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+dotenv.config()
+
+connectDB()
+
+/*
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+
+import express from 'express';
+const app = express();
+
+( async () =>{
+    try {
+        await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`)
+
+        app.on('error', (error) => {
+            console.error('Connection error',error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error('Error', error);
+        throw error;
+    }
+})();
+*/
+
+
+
+
