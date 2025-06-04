41b3871e3b667a47aea3fc64fcfd4f4d3b1874e6
Vishal Bhat Soori <vishalbhatsoori@gmail.com>
2025-06-04 18:40:16 +0000

    added jsonwebtoken generation methods in users.models

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -56,4 +56,30 @@ userSchema.methods.checkPswd = async function(password){
     return await bcrypt.compare(password, this.pswd);
 }
 
+userSchema.methods.generateAccessToken= function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            userName:this.userName,
+            fullName:this.fullName,
+            email:this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+        
+    )
+}
 export const User = mongoose.model('User',userSchema);
